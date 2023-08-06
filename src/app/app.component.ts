b4c498599de96fc5c21dab46ddc12aa199ed1463
Bublik Hum-num <Iris_Gonzales103@mail.ru>
2023-08-06 18:08:55 +0300

    2.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import jwt_decode from 'jwt-decode';
 
 interface Question {
     id: number;
@@ -26,7 +27,7 @@ interface Result {
 }
 interface CheckTestCompletedResponse {
     testCompleted: boolean;
-  }
+}
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -42,6 +43,7 @@ export class AppComponent {
     newPassword: string = "";
     users: User[] = [];
     testCompleted: boolean = false;
+    isAdmin:boolean=false;
 
     token: string | null = null;
     questions: Question[] = [];
@@ -59,128 +61,109 @@ export class AppComponent {
 
     ngOnInit() {
     }
-    getUsersNotTakenTest() {
-        if (this.username === 'admin') {
-            this.http
-                .get<string[]>('http://localhost:3000/usersNotTakenTest', {
-                    headers: new HttpHeaders({
-                        Authorization: `Bearer ${this.token}`,
-                    }),
-                })
-                .subscribe((data) => {
-                    this.usersNotTakenTest = data;
-                });
-        }
-    }
-    
-    handleLogin(event: Event) {
-        event.preventDefault();
+
+
+    login() {
         this.http
             .post<{ token: string }>('http://localhost:3000/login', { username: this.username, password: this.password })
-            .subscribe((data) => {
-                if (data.token) {
-                    this.token = data.token;
-                    if (this.username === 'admin') {
-                        this.showSubmitButton = false;
-                        this.http
-                            .get<Result[]>('http://localhost:3000/results', {
+            .subscribe(
+                (res) => {
+                    localStorage.setItem('token', res.token);
+
+                    if (res.token) {
+                        this.token = res.token;
+                        const decodedToken = jwt_decode<{ user_id: number; isAdmin: boolean }>(this.token);
+                        if (decodedToken.isAdmin) {//если зашёл админ
+                            this.isAdmin=true
+                            this.http.get<Result[]>('http://localhost:3000/results', {
                                 headers: new HttpHeaders({
                                     Authorization: `Bearer ${this.token}`,
                                 }),
                             })
-                            .subscribe((data) => {
-                                this.results = data; //as unknown as GroupedResults;
-                            }); this.http.get<number>('http://localhost:3000/user-count', {
+                                .subscribe((data) => {
+                                    this.results = data; //as unknown as GroupedResults;
+                                });
+                            this.http.get<number>('http://localhost:3000/user-count', {
                                 headers: new HttpHeaders({
                                     Authorization: `Bearer ${this.token}`,
                                 }),
                             })
                                 .subscribe((count) => {
                                     this.userCount = count;
-                                }); this.http.get<string[]>('http://localhost:3000/users-not-completed', {
-                                    headers: new HttpHeaders({
-                                        Authorization: `Bearer ${this.token}`,
-                                    }),
-                                })
-                                    .subscribe((users) => {
-                                        this.usersNotCompleted = users;
-                                    });
-                        this.http.get<User[]>('http://localhost:3000/users')
-                            .subscribe((users) => (this.users = users));
-
-                    } else {
-                        this.http
-                            .get<Question[]>('http://localhost:3000/test', {
+                                });
+                            this.http.get<string[]>('http://localhost:3000/users-not-completed', {
                                 headers: new HttpHeaders({
                                     Authorization: `Bearer ${this.token}`,
                                 }),
                             })
-                            .subscribe((data) => {
-                                this.questions = data;
-                                this.answers = new Array(this.questions.length).fill(undefined);
-                            }, (error) => {
-                                this.showSubmitButton = false;
+                                .subscribe((users) => {
+                                    this.usersNotCompleted = users;
+                                });
+                            this.http.get<User[]>('http://localhost:3000/users')
+                                .subscribe((users) => (this.users = users));
+                            this.http
+                                .get<string[]>('http://localhost:3000/usersNotTakenTest', {
+                                    headers: new HttpHeaders({
+                                        Authorization: `Bearer ${this.token}`,
+                                    }),
+                                })
+                                .subscribe((data) => {
+                                    this.usersNotTakenTest = data;
+                                });
+                        }
 
-                                if (error.status === 500) {
-                                    this.errorMessage = 'Internal server error';
-                                } else
-                                    if (error.status === 403) {
+                        else {
+                                
+                            this.http
+                                .get<Question[]>('http://localhost:3000/test', {
+                                    headers: new HttpHeaders({
+                                        Authorization: `Bearer ${this.token}`,
+                                    }),
+                                })
+                                .subscribe((data) => {
+                                    this.questions = data;
+                                    this.answers = new Array(this.questions.length).fill(undefined);
+                                    // this.message = "Спасибо за ваш ответ"
+                                    // this.token = null;
+                                    // this.questions = [];
+                                    // this.answers = [];
+                                    // // this.results = [];
+                                    // this.username = '';
+                                    // this.password = '';
+
+                                }, (error) => {
+                                    this.showSubmitButton = false;
+
+                                    if (error.status === 500) {
+                                        this.errorMessage = 'Internal server error';
+                                    } else if (error.status === 403) {
                                         this.errorMessage = 'You have already taken the test';
-                                        
-                                    } else
-                                        if (error.status === 401) {
-                                            this.errorMessage = 'Unauthorized: Invalid username or password.';
-                                        } else {
-                                            this.errorMessage = 'An error occurred while logging in. Please try again.';
-                                        }
-                            });
+                                    } else if (error.status === 401) {
+                                        this.errorMessage = 'Unauthorized: Invalid username or password.';
+                                    } else {
+                                        this.errorMessage = 'An error occurred while logging in. Please try again.';
+                                    }
+                                });
+                        }
                     }
                 }
-            },
-                (error) => {
-                    this.showSubmitButton = false;
-
-                    if (error.status === 500) {
-                        this.errorMessage = 'Internal server error';
-                    } else
-                        if (error.status === 403) {
-                            this.errorMessage = 'You have already taken the test';
-                            this.showSubmitButton = false;
-
-                        } else {
-                            this.errorMessage = 'An error occurred while logging in. Please try again.';
-                        }
-                })
+            );
     }
 
-
-
-    handleSubmit(event: Event) {
-        event.preventDefault();
+    onSubmit() {
         this.http
             .post(
                 'http://localhost:3000/test',
-                { answers: this.answers },
-                {
+                { answers: this.answers },                {
                     headers: new HttpHeaders({
                         'Content-Type': 'application/json',
                         Authorization: `Bearer ${this.token}`,
                     }),
                 }
             )
-        .subscribe((data) => {
-            console.log(data);
-           
-        });
-
-        this.message = "Спасибо за ваш ответ"
-        this.token = null;
-        this.questions = [];
-        this.answers = [];
-        // this.results = [];
-        this.username = '';
-        this.password = '';
-
+            .subscribe((data) => {
+                console.log(data);
+            }); location.reload();
     }
 
     changePassword(username: string, newPassword: string) {
@@ -206,4 +189,9 @@ export class AppComponent {
             .get<CheckTestCompletedResponse>(`/check-test-completed/${userId}`)
             .subscribe((response) => (this.testCompleted = response.testCompleted));
     }
+
+
+   
+    
+     
 }

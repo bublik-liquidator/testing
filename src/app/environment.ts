22673a76561101b99e7c71963b8809b7c42f9730
Bublik Hum-num <Iris_Gonzales103@mail.ru>
2023-11-20 13:51:59 +0300

    changed address

--- src/app/environment.ts
+++ src/app/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    serverUrl: 'https://test.kzkvv.me'  
+    serverUrl: 'https://test.kzkvv.me/api'  
       // serverUrl: process.env['SERVER_URL']
 
   };
